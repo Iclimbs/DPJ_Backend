6c44428c3ca775244d4b42cda352f3347e60201a
Uttam kumar Shaw <uttamkrshaw@iclimbs.com>
2025-01-06 13:37:17 +0530

    Update FileUpload.js
    Fixed MiddleWare For Image Upload

--- middleware/FileUpload.js
+++ middleware/FileUpload.js
@@ -19,38 +19,36 @@ const storage = multerS3({
   bucket: process.env.S3_BUCKET_NAME,
   contentType: multerS3.AUTO_CONTENT_TYPE,
   metadata: function (req, file, cb) {
-    cb(null, {fieldName: file.fieldname});
+    cb(null, { fieldName: file.fieldname });
   },
   key: function (req, file, cb) {
     cb(null, Date.now().toString())
   }
 })
-  
 
-  function checkFileType(file, cb) {
-    const filetypes = /jpeg|jpg|png|gif|mp4|mov|png/;
 
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+function checkFileType(file, cb) {
+  const filetypes = /jpeg|jpg|png|gif|mp4|mov|png/;
 
-    const mimetype = filetypes.test(file.mimetype);
-    
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
 
-    if (extname && mimetype) {
-      return cb(null, true);
-    } else {
-      cb('Error: Images only (jpeg, jpg, png, gif, mp4, mov, png)!');
-    }
+  const mimetype = filetypes.test(file.mimetype);
+  if (extname && mimetype) {
+
+    return cb(null, true);
+  } else {
+    cb('Error: Images only (jpeg, jpg, png, gif, mp4, mov, png)!');
   }
+}
 
-  const upload = multer({
-    storage: storage,
-    fileFilter: function (req, file, cb) {
-      checkFileType(file, cb);
-    },
-  }); 
-  
-  const uploadMiddleWare = upload
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+})
 
-  module.exports = uploadMiddleWare
- 
-  
+const uploadMiddleWare = upload
+
+module.exports = { uploadMiddleWare }
+
