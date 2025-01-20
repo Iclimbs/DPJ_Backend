0c1983844a3a6dff0445e49e2f091b8630cc385b
uttamiclimbs <uttamkrshaw@iclimbs.com>
2025-01-20 10:56:11 +0530

    Working On Subscription Plan

--- controller/subscription.js
+++ controller/subscription.js
@@ -12,4 +12,31 @@ SubscriptionRouter.post("/create", async (req, res) => {
     }
 })
 
+SubscriptionRouter.get("/all", async (req, res) => {
+    try {
+        const subscriptions = await SubscriptionModel.find();
+        res.json({ status: "success", data: subscriptions });
+    } catch (error) {
+        res.json({ status: "error", message: `Failed To Fetch Subscriptions ${error}` });
+    }
+})
+
+SubscriptionRouter.get("/:id", async (req, res) => {
+    try {
+        const subscription = await SubscriptionModel.findById(req.params.id);
+        res.json({ status: "success", data: subscription });
+    } catch (error) {
+        res.json({ status: "error", message: `Failed To Fetch Subscription ${error}` });
+    }
+});
+
+SubscriptionRouter.patch("/:id", async (req, res) => {
+    try {
+        await SubscriptionModel.findByIdAndUpdate(req.params.id, req.body);
+        res.json({ status: "success", message: "Subscription updated successfully" });
+    } catch (error) {
+        res.json({ status: "error", message: `Failed To Update Subscription ${error}` });
+    }
+})
+
 module.exports = { SubscriptionRouter };
