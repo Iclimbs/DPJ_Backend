3535ae2e46c8db67fa68d980ec62cddd818f75cd
uttamiclimbs <uttamkrshaw@iclimbs.com>
2025-01-28 12:39:12 +0530

    Chat Application Completed

--- controller/chat.js
+++ controller/chat.js
@@ -1,64 +1,3 @@
-// const express = require("express");
-
-// import { StreamChat } from "stream-chat"
-
-// const streamChat = StreamChat.getInstance(
-//   process.env.STREAM_API_KEY!,
-//   process.env.STREAM_PRIVATE_API_KEY!
-// )
-
-// const TOKEN_USER_ID_MAP = new Map<string, string>()
-
-// export async function userRoutes(app: FastifyInstance) {
-//   app.post<{ Body: { id: string; name: string; image?: string } }>(
-//     "/signup",
-//     async (req, res) => {
-//       const { id, name, image } = req.body
-//       if (id == null || id === "" || name == null || name === "") {
-//         return res.status(400).send
-//       }
-
-//       const existingUsers = await streamChat.queryUsers({ id })
-//       if (existingUsers.users.length > 0) {
-//         return res.status(400).send("User ID taken")
-//       }
-
-//       await streamChat.upsertUser({ id, name, image })
-//     }
-//   )
-
-//   app.post<{ Body: { id: string } }>("/login", async (req, res) => {
-//     const { id } = req.body
-//     if (id == null || id === "") {
-//       return res.status(400).send
-//     }
-
-//     const {
-//       users: [user],
-//     } = await streamChat.queryUsers({ id })
-//     if (user == null) return res.status(401).send()
-
-//     const token = streamChat.createToken(id)
-//     TOKEN_USER_ID_MAP.set(token, user.id)
-
-//     return {
-//       token,
-//       user: { name: user.name, id: user.id, image: user.image },
-//     }
-//   })
-
-//   app.post<{ Body: { token: string } }>("/logout", async (req, res) => {
-//     const token = req.body.token
-//     if (token == null || token === "") return res.status(400).send()
-
-//     const id = TOKEN_USER_ID_MAP.get(token)
-//     if (id == null) return res.status(400).send()
-
-//     await streamChat.revokeUserToken(id, new Date())
-//     TOKEN_USER_ID_MAP.delete(token)
-//   })
-// }
-
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const ChatRouter = express.Router();
@@ -68,7 +7,6 @@ const streamChat = StreamChat.getInstance(
   process.env.STREAM_API_KEY,
   process.env.STREAM_PRIVATE_API_KEY,
 );
-console.log("stream ", streamChat);
 
 const TOKEN_USER_ID_MAP = new Map();
 
@@ -79,31 +17,39 @@ ChatRouter.use(express.json());
 ChatRouter.post("/signup", async (req, res) => {
   const token = req.headers.authorization.split(" ")[1];
   const decoded = jwt.verify(token, "Authentication");
-  // const { id name, profile } = decoded;
   const id = decoded._id;
   const name = decoded.name;
   const image = decoded.profile;
-  console.log("decoded", decoded);
-  // const { id, name, image } = req.body;
 
   if (!id || !name) {
-    return res.status(400).send("Missing required fields");
+    return res.json({
+      status: "error",
+      message: "User Id Required For SignUp",
+    });
   }
 
   try {
     const existingUsers = await streamChat.queryUsers({ id });
 
     if (existingUsers.users.length > 0) {
-      return res.status(400).send("User ID taken");
+      return res.json({
+        status: "error",
+        message: "You have already Registered With This User Id",
+      });
+      // return res.status(400).send("User ID taken");
     }
 
     await streamChat.upsertUser({ id, name, image });
-
-    res.status(201).send(); // Added success response
+    return res.json({ status: "success" });
+    // res.status(201).send(); // Added success response
   } catch (error) {
     console.error(error);
+    return res.json({
+      status: "error",
+      message: `Stream SignUp Failed  error.message`,
+    });
 
-    res.status(500).send("Internal server error");
+    // res.status(500).send("Internal server error");
   }
 });
 
@@ -112,13 +58,15 @@ ChatRouter.post("/login", async (req, res) => {
   const decoded = jwt.verify(token, "Authentication");
   // const { id: _id, name, image: profile } = decoded;
   const id = decoded._id;
-  const name = decoded.name;
-  const image = decoded.profile;
 
   // const { id } = req.body;
 
   if (!id) {
-    return res.status(400).send("Missing user ID");
+    return res.json({
+      status: "error",
+      message: "User Id is Required For Login",
+    });
+    // return res.status(400).send("Missing user ID");
   }
 
   try {
@@ -127,12 +75,21 @@ ChatRouter.post("/login", async (req, res) => {
     const user = users[0];
 
     if (!user) {
-      return res.status(401).send("Unauthorized");
+      return res.json({
+        status: "error",
+        message: "You are Unauthorized to access this feature",
+      });
+
+      // return res.status(401).send("Unauthorized");
     }
 
     const token = streamChat.createToken(id);
 
     TOKEN_USER_ID_MAP.set(token, user.id);
+    return res.json({
+      status: "success",
+      data: { token: token, user: user.name, id: user.id, image: user.image },
+    });
 
     res.json({
       token,
@@ -141,8 +98,12 @@ ChatRouter.post("/login", async (req, res) => {
     });
   } catch (error) {
     console.error(error);
+    return res.json({
+      status: "error",
+      message: `Internal Server Error ${error.message}`,
+    });
 
-    res.status(500).send("Internal server error");
+    // res.status(500).send("Internal server error");
   }
 });
 
@@ -150,25 +111,43 @@ ChatRouter.post("/logout", async (req, res) => {
   const { token } = req.body;
 
   if (!token) {
-    return res.status(400).send("Missing token");
+    return res.json({
+      status: "error",
+      message: `Token Required`,
+    });
+
+    // return res.status(400).send("Missing token");
   }
 
   try {
     const id = TOKEN_USER_ID_MAP.get(token);
 
     if (!id) {
-      return res.status(400).send("Invalid token");
+      return res.json({
+        status: "error",
+        message: `Invalid Token`,
+      });
+
+      // return res.status(400).send("Invalid token");
     }
 
     await streamChat.revokeUserToken(id, new Date());
 
     TOKEN_USER_ID_MAP.delete(token);
+    return res.json({
+      status: "success",
+      message: `Logged out successfully`,
+    });
 
-    res.status(200).send("Logged out successfully");
+    // res.status(200).send("Logged out successfully");
   } catch (error) {
     console.error(error);
+    return res.json({
+      status: "error",
+      message: `Internal Server Error ${error.message}`,
+    });
 
-    res.status(500).send("Internal server error");
+    // res.status(500).send("Internal server error");
   }
 });
 
