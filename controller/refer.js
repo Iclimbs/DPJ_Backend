a6790a806d7b95dc5607a2dcf901ada354ef84ed
uttamiclimbs <uttamkrshaw@iclimbs.com>
2025-02-02 22:51:18 +0530

    Refer & Earn Function Completed

--- controller/refer.js
+++ controller/refer.js
@@ -9,19 +9,15 @@ const generateUniqueId = require('generate-unique-id');
 const generateReferenceId = async (props) => {
     try {
         const newid = new ReferModel({
-            userId: props._id,
+            userId: props.userId,
             referId: generateUniqueId({
-                includeSymbols: ['@', '#', '|'],
-                excludeSymbols: ['0'],
                 length: 6,
                 useLetters: true,
                 useNumbers: true
-            })
+            }).toUpperCase()
         })
-
-        await newid.save()
-        return { status: "success" }
-
+        const user = await newid.save()
+        return { status: "success", data: user }
     } catch (error) {
         return { status: 'error', message: `${error.message}` }
     }
@@ -32,20 +28,18 @@ ReferRouter.get("/getlist", async (req, res) => {
     const token = req.headers.authorization.split(" ")[1];
     const decoded = jwt.verify(token, "Authentication");
     try {
-        const users = await ReferModel.aggregate([{ $match: { userId: new mongoose.Types.ObjectId(decoded._id) } }])
-        console.log("users ",users)
+        const users = await ReferModel.aggregate([{ $match: { userId: new mongoose.Types.ObjectId(decoded._id) } },
+        { $lookup: { from: "users", localField: 'registeredBy', foreignField: '_id', pipeline: [{ $project: { _id: 1, name: 1, email: 1, category: 1, profile: 1 } }], as: 'registeredUserDetails' } }])
         if (users.length !== 0) {
             return res.json({ status: "success", data: users[0] })
         } else {
-            const newUser = await generateReferenceId({ _id: decoded._id })
-
+            const newUser = await generateReferenceId({ userId: decoded._id })
+            console.log("new user",newUser);
+            
             if (newUser.status === 'success') {
-                const users = await ReferModel.aggregate([{ $match: new mongoose.Types.ObjectId(decoded._id) }])
-                return res.json({ status: "success", data: users[0] })
-
+                return res.json({ status: "success", data: newUser.data })
             } else {
-                return res.json({ status: "error", message: `Unable To Fetch Refer & Earn Details` })
-
+                return res.json({ status: "error", message: `Unable To Fetch Refer & Earn Details ${newUser.message}` })
             }
         }
     } catch (error) {
