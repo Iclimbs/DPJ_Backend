2c04102a641dcbf0f36319ec4a1f7cf1b49d23b2
uttamiclimbs <uttamkrshaw@iclimbs.com>
2025-02-03 19:33:52 +0530

    Working On Google Auth

--- service/googleAuth.js
+++ service/googleAuth.js
@@ -8,7 +8,7 @@ const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5173/auth/google/callback",
+    callbackURL: "http://backend.dpjhub.com/auth/google/callback",
     passReqToCallback: true,
 },
     function (request, accessToken, refreshToken, profile, done) {
